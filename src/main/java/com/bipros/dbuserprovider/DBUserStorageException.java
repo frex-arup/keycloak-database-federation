af19ab39a82ee6d0f1fa8352a7cfb9aece7ebf53
Arupananda Pal <arupananda.pal@bipros.com>
2023-06-30 08:53:50 +0530

    package renamed

---
+++ src/main/java/com/bipros/dbuserprovider/DBUserStorageException.java
@@ -0,0 +1,16 @@
+package com.bipros.dbuserprovider;
+
+public class DBUserStorageException extends RuntimeException {
+
+    public DBUserStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DBUserStorageException(Throwable cause) {
+        super(cause);
+    }
+
+    public DBUserStorageException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
